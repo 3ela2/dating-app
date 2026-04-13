05db6a1e8c26bd0a49776eef3d9ad6099903931b
3ela2 <cds.ahmedalaa10784@alexu.edu.eg>
2026-04-13 11:56:48 +0200

    End of sec 6

--- DatingApp/client/src/layout/nav/nav.ts
+++ DatingApp/client/src/layout/nav/nav.ts
@@ -1,16 +1,19 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastService } from '../../core/services/toast-service';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule,RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
 export class Nav {
   protected accountService= inject(AccountService);
-
+  protected router= inject(Router);
+  protected toastService= inject(ToastService);
   protected creds: any= {}
   
 
@@ -18,14 +21,19 @@ export class Nav {
     this.accountService.login(this.creds).subscribe({
       next: result => {
         console.log(result);
+        this.toastService.success('Login successful');
+        this.router.navigateByUrl('/members');
         this.creds = {};
       },
-      error: error => alert(error.message)
+      error: error => {
+        this.toastService.error(error.error);
+      }
     })
   }
-
+  
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
