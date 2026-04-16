a1463e2fe8d26a8c6886dc3f65a6bf74385af91c
3ela2 <cds.ahmedalaa10784@alexu.edu.eg>
2026-04-16 14:12:21 +0200

    End of sec 8

--- DatingApp/API/Controllers/MembersController.cs
+++ DatingApp/API/Controllers/MembersController.cs
@@ -1,28 +1,44 @@
 using API.Data;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    
-    public class MembersController(AppDbContext context) : BaseApiController
+
+    [Authorize]
+    public class MembersController(IMemberRepository memberRepository) : BaseApiController
     {
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+        public async Task<ActionResult<IReadOnlyList<Member>>> GetMembers()
         {
-            var members = await context.Users.ToListAsync();
-            return members;
+            return Ok(await memberRepository.GetMembersAsync());
+
         }
-        
-        [Authorize]
+
         [HttpGet("{id}")] //localhost:5001/api/members/bob-id
-        public async Task<ActionResult<AppUser>> GetMember(string id)
+        public async Task<ActionResult<Member>> GetMember(string id)
         {
-            var member = await context.Users.FindAsync(id);
+            var member = await memberRepository.GetMemberByIdAsync(id);
             if (member == null) return NotFound();
             return member;
         }
+
+        [HttpGet("{id}/photos")]
+        public async Task<ActionResult<IReadOnlyList<Photo>>> GetPhotosForMember(string id)
+        {
+            return Ok(await memberRepository.GetPhotosForMemberAsync(id));
+        }
+
+        // [HttpPut]
+        // public async Task<ActionResult> UpdateMember(string id, Member member)
+        // {
+        //     if (id != member.Id) return BadRequest("Id does not match member");
+        //     memberRepository.Update(member);
+        //     if (await memberRepository.SaveAllAsync()) return NoContent();
+        //     throw new Exception("Failed to update member");
+        // }
     }
 }
